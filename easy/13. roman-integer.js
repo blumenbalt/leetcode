52b2daa875288fb2a27c55e6bdc7d296edda2173
blumenbalt <arthurhaguiuda@gmail.com>
2024-05-02 15:14:09 -0300

    feat: another solution

--- easy/13. roman-integer.js
+++ easy/13. roman-integer.js
@@ -13,50 +13,17 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000,
   };
+
   let result = 0;
   for (let i = 0; i < reverseRomanArray.length; i++) {
     const letter = reverseRomanArray[i];
-    if (letter === "V" && reverseRomanArray[i + 1] === "I") {
-      result = result + 4;
-
+    const nextLetter = reverseRomanArray[i + 1];
+    if (nextLetter && romanMap[letter] > romanMap[nextLetter]) {
+      result = result + (romanMap[letter] - romanMap[nextLetter]);
       reverseRomanArray.splice(i + 1, 1);
       continue;
     }
 
-    if (letter === "X" && reverseRomanArray[i + 1] === "I") {
-      result = result + 9;
-
-      reverseRomanArray.splice(i + 1, 1);
-      continue;
-    }
-
-    if (letter === "L" && reverseRomanArray[i + 1] === "X") {
-      result = result + 40;
-
-      reverseRomanArray.splice(i + 1, 1);
-      continue;
-    }
-
-    if (letter === "C" && reverseRomanArray[i + 1] === "X") {
-      result = result + 90;
-
-      reverseRomanArray.splice(i + 1, 1);
-      continue;
-    }
-
-    if (letter === "D" && reverseRomanArray[i + 1] === "C") {
-      result = result + 400;
-
-      reverseRomanArray.splice(i + 1, 1);
-      continue;
-    }
-
-    if (letter === "M" && reverseRomanArray[i + 1] === "C") {
-      result = result + 900;
-
-      reverseRomanArray.splice(i + 1, 1);
-      continue;
-    }
     result = result + romanMap[letter];
   }
 
